aeb3825ca5f4fa0eb3e41a0d6abc20837d57a051
kylebaker1999 <kylebaker1999@gmail.com>
2022-11-10 12:54:10 -0700

    11/10

--- proove/molarmass.py
+++ proove/molarmass.py
@@ -1,122 +1,126 @@
+ATOMIC_SYMBOL_INDEX = 0
+ELEMENT_NAME_INDEX = 1
+atomicmass_INDEX = 2
 
-# element = {"symbol": "Ac",
-# "name": "Actinium",
-# "mass": 227}
+def main():
 
-# print(element["symbol"])
+    chemical_formula = input("Enter the molecular formula of the sample: ")
 
-# element2 = {"symbol": "Ag",
-# "name": "Silver",
-# "mass": 107.8682}
-
-# elements = [element, elementt[(elements:
+    grams = float(input("Enter the mass in grams of the sample: "))
+     
+    periodic_table = make_periodic_table()
+    symbol_quantity_list=parse_formula(chemical_formula, periodic_table)
+    molar_mass = compute_molar_mass(symbol_quantity_list, periodic_table)
+    # Print molar mass
+    print(f"{molar_mass:.2f} grams/moles")
+    
+    # Compute moles
+    molar_mass = grams / molar_mass  
+    print(f"{molar_mass:.4f} moles")
+    # print(moles)
 
 def make_periodic_table():
     periodic_table_dict = {
-        # [symbol, name, atomic_mass]
-        "Ac": ["Actinium", 227],
-        "Ag": ["Silver", 107.8682],
-        "Al": ["Aluminum", 26.9815386],
-        "Ar": ["Argon",	39.948],
-        "As": ["Arsenic",	74.9216],
-        "At": ["Astatine",	210],
-        "Au": ["Gold",	196.966569],
-        "B": ["Boron",	10.811],
-        "Ba": ["Barium",	137.327],
-        "Be": ["Beryllium",	9.012182],
-        "Bi": ["Bismuth",	208.9804],
-        "Br": ["Bromine",	79.904],
-        "C":  ["Carbon",	12.0107],
-        "Ca": ["Calcium",	40.078],
-        "Cd": ["Cadmium",	112.411],
-        "Ce": ["Cerium",	140.116],
-        "Cl": ["Chlorine",	35.453],
-        "Co": ["Cobalt",	58.933195],
-        "Cr": ["Chromium",	51.9961],
-        "Cs": ["Cesium",	132.9054519],
-        "Cu": ["Copper",	63.546],
-        "Dy": ["Dysprosium",	162.5],
-        "Er": ["Erbium",	167.259],
-        "Eu": ["Europium",	151.964],
-        "F":  ["Fluorine",	18.9984032],
-        "Fe": ["Iron",	55.845],
-        "Fr": ["Francium",	223],
-        "Ga": ["Gallium",	69.723],
-        "Gd": ["Gadolinium",	157.25],
-        "Ge": ["Germanium",	72.64],
-        "H": ["Hydrogen",	1.00794],
-        "He": ["Helium",	4.002602],
-        "Hf": ["Hafnium",	178.49],
-        "Hg": ["Mercury",	200.59],
-        "Ho": ["Holmium",	164.93032],
-        "I": ["Iodine",	126.90447],
-        "In": ["Indium",	114.818],
-        "Ir": ["Iridium",	192.217],
-        "K": ["Potassium",	39.0983],
-        "Kr": ["Krypton",	83.798],
-        "La": ["Lanthanum",	138.90547],
-        "Li": ["Lithium",	6.941],
-        "Lu": ["Lutetium",	174.9668],
-        "Mg": ["Magnesium",	24.305],
-        "Mn": ["Manganese",	54.938045],
-        "Mo": ["Molybdenum",	95.96],
-        "N": ["Nitrogen",	14.0067],
-        "Na": ["Sodium",	22.98976928],
-        "Nb": ["Niobium",	92.90638],
-        "Nd": ["Neodymium",	144.242],
-        "Ne": ["Neon",	20.1797],
-        "Ni": ["Nickel",	58.6934],
-        "Np": ["Neptunium",	237],
-        "O": ["Oxygen",	15.9994],
-        "Os": ["Osmium",	190.23],
-        "P": ["Phosphorus",	30.973762],
-        "Pa": ["Protactinium",	231.03588],
-        "Pb": ["Lead",	207.2],
-        "Pd": ["Palladium",	106.42],
-        "Pm": ["Promethium",	145],
-        "Po": ["Polonium",	209],
-        "Pr": ["Praseodymium",	140.90765],
-        "Pt": ["Platinum",	195.084],
-        "Pu": ["Plutonium",	244],
-        "Ra": ["Radium",	226],
-        "Rb": ["Rubidium",	85.4678],
-        "Re": ["Rhenium",	186.207],
-        "Rh": ["Rhodium",	102.9055],
-        "Rn": ["Radon",	222],
-        "Ru": ["Ruthenium",	101.07],
-        "S":  ["Sulfur",	32.065],
-        "Sb": ["Antimony",	121.76],
-        "Sc": ["Scandium",	44.955912],
-        "Se": ["Selenium",	78.96],
-        "Si": ["Silicon",	28.0855],
-        "Sm": ["Samarium",	150.36],
-        "Sn": ["Tin",	118.71],
-        "Sr": ["Strontium",	87.62],
-        "Ta": ["Tantalum",	180.94788],
-        "Tb": ["Terbium",	158.92535],
-        "Tc": ["Technetium",	98],
-        "Te": ["Tellurium",	127.6],
-        "Th": ["Thorium",	232.03806],
-        "Ti": ["Titanium",	47.867],
-        "Tl": ["Thallium",	204.3833],
-        "Tm": ["Thulium",	168.93421],
-        "U": ["Uranium",	238.02891],
-        "V": ["Vanadium",	50.9415],
-        "W": ["Tungsten",	183.84],
-        "Xe": ["Xenon",	131.293],
-        "Y": ["Yttrium",	88.90585],
-        "Yb": ["Ytterbium",	173.054],
-        "Zn": ["Zinc",	65.38],
-        "Zr": ["Zirconium",	91.22]
+        # ["symbol 0", "name 1", "atomicmass 2"]
+        "Ac":   ["Actinium", 227],
+        "Ag":   ["Silver", 107.8682],
+        "Al":   ["Aluminum", 26.9815386],
+        "Ar":   ["Argon", 39.948],
+        "As":   ["Arsenic", 74.9216],
+        "At":   ["Astatine",	210],
+        "Au":   ["Gold",	196.966569],
+        "B":    ["Boron", 10.811],
+        "Ba":   ["Barium", 137.327],
+        "Be":   ["Beryllium", 9.012182],
+        "Bi":   ["Bismuth", 208.9804],
+        "Br":   ["Bromine", 79.904],
+        "C":    ["Carbon", 12.0107],
+        "Ca":   ["Calcium", 40.078],
+        "Cd":   ["Cadmium", 112.411],
+        "Ce":   ["Cerium", 140.116],
+        "Cl":   ["Chlorine", 35.453],
+        "Co":   ["Cobalt", 58.933195],
+        "Cr":   ["Chromium", 51.9961],
+        "Cs":   ["Cesium", 132.9054519],
+        "Cu":   ["Copper", 63.546],
+        "Dy":   ["Dysprosium", 162.5],
+        "Er":   ["Erbium", 167.259],
+        "Eu":   ["Europium", 151.964],
+        "F":    ["Fluorine",	18.9984032],
+        "Fe":   ["Iron",	55.845],
+        "Fr":   ["Francium",	223],
+        "Ga":   ["Gallium",	69.723],
+        "Gd":   ["Gadolinium", 157.25],
+        "Ge":   ["Germanium", 72.64],
+        "H":    ["Hydrogen",	1.00794],
+        "He":	["Helium", 4.002602],
+        "Hf":	["Hafnium", 178.49],
+        "Hg":	["Mercury", 200.59],
+        "Ho":	["Holmium", 164.93032],
+        "I":	["Iodine", 126.90447],
+        "In":	["Indium", 114.818],
+        "Ir":	["Iridium", 192.217],
+        "K":	["Potassium", 39.0983],
+        "Kr":	["Krypton", 83.798],
+        "La":	["Lanthanum", 138.90547],
+        "Li":	["Lithium", 6.941],
+        "Lu":	["Lutetium", 174.9668],
+        "Mg":	["Magnesium", 24.305],
+        "Mn":	["Manganese", 54.938045],
+        "Mo":	["Molybdenum", 95.96],
+        "N":	["Nitrogen",	14.0067],
+        "Na":	["Sodium", 22.98976928],
+        "Nb":	["Niobium", 92.90638],
+        "Nd":	["Neodymium", 144.242],
+        "Ne":	["Neon",	20.1797],
+        "Ni":	["Nickel", 58.6934],
+        "Np":	["Neptunium", 237],
+        "O":	["Oxygen", 15.9994],
+        "Os":	["Osmium", 190.23],
+        "P":	["Phosphorus", 30.973762],
+        "Pa":	["Protactinium", 231.03588],
+        "Pb":	["Lead",	207.2],
+        "Pd":	["Palladium", 106.42],
+        "Pm":	["Promethium", 145],
+        "Po":	["Polonium",	209],
+        "Pr":	["Praseodymium",	140.90765],
+        "Pt":	["Platinum",	195.084],
+        "Pu":	["Plutonium", 244],
+        "Ra":   ["Radium", 226],
+        "Rb":	["Rubidium",	85.4678],
+        "Re":	["Rhenium", 186.207],
+        "Rh":	["Rhodium", 102.9055],
+        "Rn":	["Radon", 222],
+        "Ru":	["Ruthenium", 101.07],
+        "S":	["Sulfur",  32.065],
+        "Sb":	["Antimony",	121.76],
+        "Sc":	["Scandium",	44.955912],
+        "Se":	["Selenium",	78.96],
+        "Si":	["Silicon",     28.0855],
+        "Sm":	["Samarium",    150.36],
+        "Sn":	["Tin", 118.71],
+        "Sr":	["Strontium", 87.62],
+        "Ta":	["Tantalum",	180.94788],
+        "Tb":	["Terbium", 158.92535],
+        "Tc":	["Technetium", 98],
+        "Te":	["Tellurium", 127.6],
+        "Th":	["Thorium", 232.03806],
+        "Ti":	["Titanium",	47.867],
+        "Tl":	["Thallium",	204.3833],
+        "Tm":	["Thulium", 168.93421],
+        "U":	["Uranium", 238.02891],
+        "V":	["Vanadium",	50.9415],
+        "W":    ["Tungsten", 183.84],
+        "Xe":   ["Xenon", 131.293],
+        "Y":    ["Yttrium", 88.90585],
+        "Yb":   ["Ytterbium", 173.054],
+        "Zn":   ["Zinc", 65.38],
+        "Zr":   ["Zirconium", 91.224]
     }
-            
+    
+    return periodic_table_dict
 
 
-#periodic_table_dict = make_periodic_table()
-
-# total_mass = 0
-# for element in periodic_table_dict:
-#     total_mass += element["mass"]
 
 class FormulaError(ValueError):
     """FormulaError is the type of error that the parse_formula
@@ -130,7 +134,6 @@ def parse_formula(formula, periodic_table_dict):
     in the molecule. For example, this function will convert
     "H2O" to [["H", 2], ["O", 1]] and
     "PO4H2(CH2)12CH3" to [["P", 1], ["O", 4], ["H", 29], ["C", 13]]
-
     Parameters
         formula is a string that contains a chemical formula
         periodic_table_dict is the compound dictionary returned
@@ -219,7 +222,7 @@ def parse_formula(formula, periodic_table_dict):
 
 # Indexes for inner lists in the periodic table
 NAME_INDEX = 0
-ATOMIC_MASS_INDEX = 1
+atomicmass_INDEX = 1
 
 # Indexes for inner lists in a symbol_quantity_list
 SYMBOL_INDEX = 0
@@ -229,7 +232,6 @@ QUANTITY_INDEX = 1
 def compute_molar_mass(symbol_quantity_list, periodic_table_dict):
     """Compute and return the total molar mass of all the
     elements listed in symbol_quantity_list.
-
     Parameters
         symbol_quantity_list is a compound list returned
             from the parse_formula function. Each small
@@ -239,20 +241,26 @@ def compute_molar_mass(symbol_quantity_list, periodic_table_dict):
             returned from make_periodic_table.
     Return: the total molar mass of all the elements in
         symbol_quantity_list.
-
     For example, if symbol_quantity_list is [["H", 2], ["O", 1]],
     this function will calculate and return
-    atomic_mass("H") * 2 + atomic_mass("O") * 1
+    atomicmass("H") * 2 + atomicmass("O") * 1
     1.00794 * 2 + 15.9994 * 1
     18.01528
     """
-    # Do the following for each each inner list in the
-    # compound symbol_quantity_list:
-        # Separate the inner list into symbol and quantity.
-        # Get the atomic mass for the symbol from the dictionary.
-        # Multiply the atomic mass by the quantity.
-        # Add the product into the total molar mass.
-
-    # Return the total molar mass.
-    return
+    molar_mass = 0.0
     
+    for inner_list in symbol_quantity_list:    
+
+        symbol = inner_list[SYMBOL_INDEX]
+        quantity = inner_list[QUANTITY_INDEX]
+        
+        atomicmass = periodic_table_dict[symbol][atomicmass_INDEX]
+        product = atomicmass * quantity
+        molar_mass += product
+        
+     
+        return molar_mass
+    
+# execute the code with main
+if __name__ == "__main__":
+    main()
